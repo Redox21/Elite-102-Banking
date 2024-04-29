0ef5c9353bc15e4220973ce7408829519b988c03
Redox21 <serrano.topilot@gmail.com>
2024-04-29 00:59:38 -0500

    [commit message]

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ import functions as fun
 # testing python functions
 # TESTING HAS BEEN LIMITED DUE TO LACK OF ACTUAL CONNECTION TO DATABASE until recent update
 
+# copy message for commiting changes.
+# git commit -m "[commit message]"
+
 class TestBanking(unittest.TestCase):
     
     def test_account_creation(self):
